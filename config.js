b84021eeaa6246839e6d136d47499f46c72a7211
Rio060708 <77329595+Rio060708@users.noreply.github.com>
2022-02-03 16:04:33 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8lswsOKDFWNyjW-YvnyjNuWWFHlW2QGQ",
+  authDomain: "ride-app-eca64.firebaseapp.com",
+  projectId: "ride-app-eca64",
+  storageBucket: "ride-app-eca64.appspot.com",
+  messagingSenderId: "894739607429",
+  appId: "1:894739607429:web:1a947d3ad72ff96a5e5eec"
+};
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
